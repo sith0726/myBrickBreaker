cd89bbd5ee40d53d6a032b95a16dfcef9ec81e76
ankhangkieu <ankhangkieu@gmail.com>
2016-11-12 12:25:39 -0800

    BEst Current

--- js/Main.js
+++ js/Main.js
@@ -1,8 +1,11 @@
+//Stage
 var stage;
 
+//Screen data
 var screen_width = 500;
 var screen_height = 600;
 
+//Speed Value
 var default_circle_speed = 2;
 var max_speed = 3;
 
@@ -10,28 +13,39 @@ var speed_x_circle = default_circle_speed;
 var speed_y_circle = default_circle_speed;
 var speed_x_rect = 0;
 
+//Shape
 var circle;
 var rect;
 var blocks;
 
+//Shape data
 var circle_radius = 10;
 var rect_width = 100;
 var rect_height = 10;
-var block_width = 50;
+var block_width = 100;
 var block_height = 50;
 
-var block_per_row = 10;
+//Number of blocks
+var block_per_row = 50;
 var block_per_col = 4;
 var number_of_blocks = block_per_row*block_per_col;
 
+//Pictures
 var Trump1;
 var Trump2;
+var press_to_continue;
 
+//Win - Lose
 var win;
 var lose;
 
+//Preloader
 var preloader;
 
+//File Storage
+var manifest;
+
+//Document Manipulation
 document.onkeydown = handleKeyDown;
 document.onkeyup = handleKeyUp;
 
@@ -48,7 +62,8 @@ function init(){
         {src:"resources/brick.png", id:"brick"},
         {src:"resources/start.png", id:"start"},
         {src:"resources/paddle.png", id:"rect"},
-        {src:"resources/ball.png", id:"circle"},
+        {src:"resources/bala.png", id:"circle"},
+        {src:"resources/press_to_continue.png", id:"press_to_continue"},
         {src:"resources/Trump1.png", id:"Trump1"},
         {src:"resources/Trump2.png", id:"Trump2"},
         {src:"resources/win.png", id:"win"},
@@ -58,15 +73,15 @@ function init(){
     ];
     
     preloader = new createjs.LoadQueue(false);
-    preloader.addEventListener("complete", handleComplete);
+    preloader.addEventListener("complete", loading);
     preloader.loadManifest(manifest, true, "./");
 
     
     createjs.Ticker.addEventListener("tick", tick);
     createjs.Ticker.setInterval(10);
     this.slow_down = 1;
+    
     function tick(){
-
         if(this.slow_down%500 === 0){
             speed_x_circle = speed_x_circle*9/10;
             this.slow_down = 0;
@@ -81,30 +96,37 @@ function init(){
 
 }
  
-function handleComplete() {
-    var matrix1 = new createjs.Matrix2D();
-    matrix1.scale(4, 7);
-
-
+function loading() {
+    var bg_scale = new createjs.Matrix2D();
+    bg_scale.scale(4, 7);
     bg = new createjs.Shape();
-    bg.graphics.beginBitmapFill(preloader.getResult("bg"), "no-repeat", matrix1).drawRect(0,0,500,600);
+    bg.graphics.beginBitmapFill(preloader.getResult("bg"), "no-repeat", bg_scale).drawRect(0,0,500,600);
     stage.addChild(bg);
     stage.update();
     
+    var press_scale = new createjs.Matrix2D();
+    press_scale.scale(0.48, 0.5);
+    press_to_continue = new createjs.Shape();
+    press_to_continue.graphics.beginBitmapFill(preloader.getResult("press_to_continue"), "no-repeat", press_scale).drawRect(0,0,500,600);
+    press_to_continue.y = 300
+    stage.addChild(press_to_continue);
+    stage.update();
+    
 
-    var matrix2 = new createjs.Matrix2D();
-    matrix2.scale(0.5, 0.5);
 
+    var Trump1_scale = new createjs.Matrix2D();
+    Trump1_scale.scale(0.5, 0.5);
     Trump1 = new createjs.Shape();
-    Trump1.graphics.beginBitmapFill(preloader.getResult("Trump1"), "no-repeat", matrix2).drawRect(0,0,300,200);
+    Trump1.graphics.beginBitmapFill(preloader.getResult("Trump1"), "no-repeat", Trump1_scale).drawRect(0,0,300,200);
     Trump1.x = 100;
     Trump1.y = 0;
     stage.addChild(Trump1);
     stage.update();
          
-    
+    var circle_scale = new createjs.Matrix2D();
+    circle_scale.scale(1, 1);
     circle = new createjs.Shape();
-    circle.graphics.beginBitmapFill(preloader.getResult("circle")).drawCircle(0,0,circle_radius);
+    circle.graphics.beginFill(preloader.getResult("circle"), "no-repeat", circle_scale).drawCircle(0,0, circle_radius);
     circle.x = screen_width/2;
     circle.y = screen_height/2;
     stage.addChild(circle);
@@ -116,10 +138,13 @@ function handleComplete() {
     rect.y = screen_height - rect_height - 10;
     stage.addChild(rect);
     stage.update();
+
+    var block_scale = new createjs.Matrix2D();
+    block_scale.scale(1, 1);
     blocks = [];
     for(var i = 0; i < number_of_blocks; i++){
         var brick = new createjs.Shape();
-        brick.graphics.beginBitmapFill(preloader.getResult("brick")).drawRoundRect(0,0,block_width,block_height,5);
+        brick.graphics.beginBitmapFill(preloader.getResult("brick")).drawRoundRect(0,0,block_width,block_height,10);
         brick.x = block_width*(i%block_per_row);
         brick.y = block_height*Math.floor(i/block_per_row);
         blocks.push(brick);
@@ -131,9 +156,17 @@ function handleComplete() {
 function updateCircle(circle){
     if(circle.x >= screen_width - circle_radius || circle.x <= circle_radius){
         speed_x_circle = -speed_x_circle;
+        if(circle.x >= screen_width - circle_radius)
+            circle.x = screen_width - circle_radius;
+        else
+            circle.x = circle_radius;
     }
     if(circle.y >= screen_height - circle_radius || circle.y <= circle_radius){
         speed_y_circle = -speed_y_circle;
+        if (circle.y >= screen_height - circle_radius)
+            circle.y = screen_height - circle_radius;
+        else
+            circle.y <= circle_radius;
     }
     circle.x += speed_x_circle;
     circle.y += speed_y_circle;
@@ -162,7 +195,15 @@ function hitTest(stage, circle, rect, blocks){
     if(circle.x >= rect.x && circle.x <= rect.x+rect_width){
         if(rect.y -circle.y <= circle_radius){
             speed_y_circle = -speed_y_circle;
-            speed_x_circle += speed_x_rect/4;
+            speed_x_circle += speed_x_rect/3;
+            if(speed_x_circle > max_speed)
+                speed_x_circle = max_speed;
+            circle.y = rect.y - circle_radius;
+        }
+    }
+    else if(circle.y > rect.y && circle.y <rect.y + rect_height){
+        if((rect.x <= circle.x + circle_radius && rect.x > circle.x) || (rect.x + rect_width < circle.x && rect.x + rect_width >= circle.x - circle_radius)){
+            speed_x_circle += speed_x_rect/2;
             if(speed_x_circle > max_speed)
                 speed_x_circle = max_speed;
         }
