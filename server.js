49a6b7415d1dfd168393dfeadd425efd4c759df7
Tin Hang Chui <sith0726@berkeley.edu>
2016-11-12 21:51:11 -0800

    skeleton of server

--- server.js
+++ server.js
@@ -29,10 +29,51 @@ Game Server
 /*
 When the user connects to server, call "handleConnection"
 function. Set up the socket.
+
+Client to Server Signal list:
+"joinGameRequest" : this singal is sent by player when he pressed multiplayer button.
+                    The server will try to match or create a lobby.
+"disconnect" :  sent by player when disconnect.
+                If player is in lobby, remove that player in lobby.
+                If player is in game, change the player's side to wall.
+"ready" :   sent by player when he/she is ready in lobby.
+            When both players are ready, the game starts.
+"move" : Sent when player moves. Contains the new position of paddle.
+
+Server to Client Signal list:
+"lobbyInfo" : Contains the info of lobby. Sent when updating lobby info.
+"playerDisconnect" : Notify that players leaves the game.
+"mapUpdate" : Contains the new position of paddles of both players.
+"serverShutDown" : server shut down.
+
+""
 */
 io.on('connection', handleConnection);
 function handleConnection(socket) {
-    
+      socket.on('joinGameRequest', handleJoinGame);
+      socket.on('disconnect', handleDisconnect);
+      socket.on('ready', handleReady);
+      socket.on('handleMove', handleMove);
+}
+
+//joinGameRequest
+function handleJoinGame(message) {
+
+}
+
+//disconnect
+function handleDisconnect(message) {
+
+}
+
+//ready
+function handleReady(message) {
+
+}
+
+//move
+function handleMove(message) {
+
 }
 
 /*
